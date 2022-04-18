500aad81beb353ee7b6430cf76f8bf896a21a90a
nahian-29 <Eatmydust19_>
2022-04-18 16:15:34 -0400

    created and updated log folders

--- tests/click_test.py
+++ tests/click_test.py
@@ -22,11 +22,36 @@ def test_error_log_file():
     logfile = os.path.exists(errorlog)
     assert logfile == True
 
-def flask_log_file():
+def test_flask_log_file():
     root = os.path.dirname(os.path.abspath(__file__))
     flasklog = os.path.join(root, '../app/logs/flask.log')
     logfile = os.path.exists(flasklog)
     assert logfile == True
+
+def test_myapp_log_file():
+    root = os.path.dirname(os.path.abspath(__file__))
+    myapplog = os.path.join(root, '../app/logs/myapp.log')
+    logfile = os.path.exists(myapplog)
+    assert logfile == True
+
+def test_request_log_file():
+    root = os.path.dirname(os.path.abspath(__file__))
+    requestlog = os.path.join(root, '../app/logs/request.log')
+    logfile = os.path.exists(requestlog)
+    assert logfile == True
+
+def test_sqlalchemy_log_file():
+    root = os.path.dirname(os.path.abspath(__file__))
+    sqlalchemylog = os.path.join(root, '../app/logs/sqlalchemy.log')
+    logfile = os.path.exists(sqlalchemylog)
+    assert logfile == True
+
+def test_werkzeug_log_file():
+    root = os.path.dirname(os.path.abspath(__file__))
+    werkzeuglog = os.path.join(root, '../app/logs/werkzeug.log')
+    logfile = os.path.exists(werkzeuglog)
+    assert logfile == True
+
 
 def test_create_database():
     response = runner.invoke(create_database)
