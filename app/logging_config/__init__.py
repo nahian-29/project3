64ebf800ab49e14561c4fc2ec70dbc1907eae84e
nahian-29 <Eatmydust19_>
2022-04-23 17:59:58 -0400

    fixing run test

--- app/logging_config/__init__.py
+++ app/logging_config/__init__.py
@@ -4,7 +4,7 @@ from logging.config import dictConfig
 import flask
 from flask import request, current_app
 
-from .log_formatters import RequestFormatter
+from app.logging_config.log_formatters import RequestFormatter
 
 
 
